b1641641b424d0b9991ea0d39bd856e4a1c90966
UmGnJin <86461489+UmGnJin@users.noreply.github.com>
2021-07-03 22:47:09 +0900

    Map generation update(fix)
    Scripts folder only.

---
+++ new Scripts/Rooms/DownStairsRoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using noname.util;
+using System.Collections.Generic;
+using noname.painters;
+
+namespace noname.rooms
+{
+    public class DownStairsRoom : Room
+    {
+
+        public DownStairsRoom()
+        {
+            width = 4;
+            height = 4;
+            DefaultSet();
+        }
+        public override void Paint(Level l)
+        {
+            DownStairRoomPainter erg = new DownStairRoomPainter();
+            erg.Paint(l, this);
+        }
+
+    }
+}
---
+++ new Scripts/Rooms/UpStairsRoom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using noname.util;
+using noname.painters;
+
+namespace noname.rooms
+{
+    public class UpStairsRoom : Room
+    {
+
+        public UpStairsRoom()
+        {
+            width = 6;
+            height =6;
+            DefaultSet();
+        }
+        public override void Paint(Level l)
+        {
+            UpStairRoomPainter erg = new UpStairRoomPainter();
+            erg.Paint(l, this);
+        }
+    }
+}
